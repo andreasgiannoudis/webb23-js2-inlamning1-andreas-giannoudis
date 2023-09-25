a6638f3c7ad42d5040bb7d554a609750bf0dd4be
andreasgiannoudis <agiannoudis@hotmail.com>
2023-09-25 09:02:33 +0200

    Fixed a bug

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,58 +14,60 @@ const formNameOfPlayer = document.querySelector('#playerForm');
 const gameContainer = document.querySelector('#gameContainer');
 const h2ChoiceInfo = document.createElement('h2');
 const h3Points = document.createElement('h3');
-const computerImg = document.querySelector('#computerImg');
 const btnStartNewGame = document.createElement('button');
 
+
 getHighscore().then(displayHighscore);
-
-formNameOfPlayer.addEventListener('submit', async event=>{
+let handleButtonClick;
+formNameOfPlayer.addEventListener('submit', async event => {
     event.preventDefault();
-    
-    playerName = document.querySelector('#playerName').value;
-    
 
-    if(playerName == '')
-    {
-        alert('you must write your name');
+    playerName = document.querySelector('#playerName').value;
+
+    if (playerName == '') {
+        alert('You must write your name to be able to play');
     }
     else {
-        
-    const h3Name = document.createElement('h3');
+
+        const h3Name = document.createElement('h3');
         formNameOfPlayer.style.visibility = 'hidden';
         h3Name.innerText = `Hello ${playerName}, let's play the rock paper scissors game!`;
-    welcomePlayer.append(h3Name);
+        welcomePlayer.append(h3Name);
+
+        const rpsImages = btnChoices.querySelectorAll('.rps');
+        rpsImages.forEach(image => {
+            image.style.pointerEvents = 'auto';
+        });
+
+
+
         
-            const rpsImages = btnChoices.querySelectorAll('.rps');
-            rpsImages.forEach(image => {
-                image.style.pointerEvents = 'auto';
-            });
-        
-        
-    
-        btnChoices.addEventListener('click', event=>{
-            if (event.target.id === "rock"){
-                play('rock');
+        handleButtonClick = (event) => {
+            event.preventDefault();
+          
+            if (event.target.id === 'rock' || event.target.id === 'paper' || event.target.id === 'scissors') {
+              const choiceOfPlayer = event.target.id;
+              play(choiceOfPlayer);
             }
-            else if (event.target.id === "paper")
-            {
-                play('paper');
-            }
-            else if (event.target.id === "scissors")
-            {
-                play('scissors');
-            }
-        })
+          };
+          
+          btnChoices.addEventListener('click', handleButtonClick);
+
+
+
+
+
+
     }
-
-    
-    
-    
-
-    
 });
 
-
+let randomRps = null;
+function generateRandomRps() {
+    console.log('generateRandomRps called');
+    const shuffledRpsArr = _.shuffle(rpsArr);
+    randomRps = shuffledRpsArr[0];
+    console.log(randomRps);
+}
 
 
 //the game algorithm
@@ -74,74 +76,51 @@ formNameOfPlayer.addEventListener('submit', async event=>{
 //if the player wins the round then the score is increasing by 1 every time
 //and it displays info about it
 //if the computer wins the round then the game ends 
-function play(choiceOfPlayer)
-{
-    const shuffledRpsArr = _.shuffle(rpsArr);
-    const randomRps = shuffledRpsArr[0];
-    //console.log('Computer chose ' + randomRps);
-    //console.log('Player chose ' + choiceOfPlayer);
+function play(choiceOfPlayer) {
+    generateRandomRps();
 
-    if (randomRps === 'rock') {
-        //console.log('rock');
-        computerImg.src = './img/rock.png';
-    }
-    else if (randomRps === 'paper') {
-        //console.log('paper');
-        computerImg.src = './img/paper.png';
-    }
-    else {
-        //console.log('scissors');
-        computerImg.src = './img/scissors.png';
-    }
-        
     
-    if (choiceOfPlayer === randomRps)
-    {
-        h2ChoiceInfo.innerHTML=''; 
-        h3Points.innerHTML='';   
+
+    if (choiceOfPlayer === randomRps) {
+
         h2ChoiceInfo.innerText = `It's a tie. You chose ${choiceOfPlayer} and Computer chose ${randomRps}`;
         h3Points.innerText = `Your score so far is: ${playerScore}`;
+        gameContainer.append(h2ChoiceInfo, h3Points);
     }
-    else if(choiceOfPlayer === "rock" && randomRps=== "scissors" ||
-    choiceOfPlayer === "scissors" && randomRps=== "paper" ||
-    choiceOfPlayer=== "paper" && randomRps === "rock")
-    {
+    else if (choiceOfPlayer === "rock" && randomRps === "scissors" ||
+        choiceOfPlayer === "scissors" && randomRps === "paper" ||
+        choiceOfPlayer === "paper" && randomRps === "rock") {
         //Player wins 
-        h2ChoiceInfo.innerHTML=''; 
-        h3Points.innerHTML=''; 
         playerScore++;
+
         h2ChoiceInfo.innerText = `You chose ${choiceOfPlayer} and Computer chose ${randomRps}`;
         h3Points.innerText = `You won this round and got 1 more point. Your score so far is: ${playerScore}`;
+        gameContainer.append(h2ChoiceInfo, h3Points);
     }
-    else 
-    {
-        h2ChoiceInfo.innerHTML=''; 
-        h3Points.innerHTML=''; 
+    else {
+        h2ChoiceInfo.innerText = '';
+        h3Points.innerText = '';
         h2ChoiceInfo.innerText = `You chose ${choiceOfPlayer} and Computer chose ${randomRps}`;
-        pComputerWon.innerText = `Computer won. Game over! Start the game again! You got ${playerScore} points`;
-        gameContainer.append(pComputerWon);
+        h3Points.innerText = `Computer won. Game over! Start the game again! You got ${playerScore} points`;
+        gameContainer.append(h2ChoiceInfo, h3Points);
 
         //disabling the imgs when the game ends
-        const rpsImages = btnChoices.querySelector('.rps');
+        const rpsImages = btnChoices.querySelectorAll('.rps');
         rpsImages.forEach(image => {
             image.style.pointerEvents = 'none';
         });
 
         //computer wins
         //so the game must end here  
-        console.log('Points before reset: ' + playerScore);
-        endGame();  
-        console.log('Points after reset: ' + playerScore);
+        //console.log('Points before reset: ' + playerScore);
+
+        endGame();
+
+        //console.log('Points after reset: ' + playerScore);
     }
-
-    gameContainer.append(h2ChoiceInfo, h3Points);
-   
-    
-
 }
 
 
-const pComputerWon = document.createElement('p');
 
 //end game means that the computer won the round the game is over
 //checking if the player has already had a highscore before
@@ -149,15 +128,14 @@ const pComputerWon = document.createElement('p');
 //if it is the first time the player plays
 //then a new object with the name and the score add to the json file in backend
 //Also it shows a start new game button and resets everything
-async function endGame()
-{  
+async function endGame() {
     const player = {
         name: playerName,
         score: playerScore
     };
 
     const previousHighscore = await getHighscoreForPlayer(playerName);
-    
+
     if (previousHighscore != undefined) {
         if (playerScore > previousHighscore.score) {
             await updateHighscore(player);
@@ -168,13 +146,14 @@ async function endGame()
     } else {
         newPlayer(player);
     }
-    
 
-    
+    btnChoices.removeEventListener('click', handleButtonClick);
+
+
     btnStartNewGame.innerText = 'Start new game';
     gameContainer.append(btnStartNewGame);
     getHighscore().then(displayHighscore);
-    btnStartNewGame.addEventListener('click', event=>{ 
+    btnStartNewGame.addEventListener('click', event => {
         resetGame();
     });
 
@@ -184,19 +163,16 @@ async function endGame()
 
 //reseting the playerScore to 0
 //also reseting the containers so that nothing from previous game is visible
-function resetGame()
-{
-    pComputerWon.innerText = '';
-    btnStartNewGame.innerHTML = '';
-    computerImg.src="./img/questionMark.png";
-    h2ChoiceInfo.innerHTML='';
-    h3Points.innerHTML='';
+function resetGame() {
+    h2ChoiceInfo.innerText = '';
+    h3Points.innerText = '';
+    btnStartNewGame.innerText = '';
     gameContainer.innerHTML = '';
-    playerName.innerHTML = '';
-    welcomePlayer.innerHTML='';
-    formNameOfPlayer.style.visibility ='';
+    playerName.innerText = '';
+    welcomePlayer.innerText = '';
+    formNameOfPlayer.style.visibility = '';
     playerScore = 0;
     formNameOfPlayer.reset();
 }
-        
+
 
